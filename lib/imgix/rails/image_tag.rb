27ad316dcc05715854e1dcdb2e93d9c7231cc89f
Samuel Giles <livelikeasam@googlemail.com>
2016-11-02 16:30:35 +0000

    Added support for lazy loading to ix_image_tag
    Produces a really low quality version to then swap out

--- lib/imgix/rails/image_tag.rb
+++ lib/imgix/rails/image_tag.rb
@@ -2,12 +2,23 @@ require "imgix/rails/tag"
 
 class Imgix::Rails::ImageTag < Imgix::Rails::Tag
   def render
-    @options[:srcset] = srcset
+    if @options[:lazy]
+      @options[:"data-srcset"] = srcset
+    else
+      @options[:srcset] = srcset
+    end
     @options[:sizes] ||= '100vw'
 
     @source = replace_hostname(@source)
     normal_opts = @options.slice!(*self.class.available_parameters)
 
-    image_tag(ix_image_url(@source, @options), normal_opts)
+    normal_url = ix_image_url(@source, @options)
+    lazy_url = ix_image_url(@source, @options.merge({ blur: 1500, auto: "format,compress", q: 20, fm: "jpg" }))
+
+    if @options[:lazy]
+      normal_opts[:"data-src"] = ix_image_url(@source, @options)
+    end
+
+    image_tag(@options[:lazy] ? lazy_url : normal_url, normal_opts)
   end
 end
